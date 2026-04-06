f02f7f6280d8ec55788bce4a5de6e08fcc2a8758
Victor Kelly <vk561244@gmail.com>
2026-04-05 23:01:31 -0600

    chore: remove build artifacts and add gitignore

--- build/CMakeFiles/RTOS.dir/src/kernel/Process.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/RTOS.dir/src/kernel/Process.cpp.obj: \
- /home/victorkelly1/C++/practicas/RTOS/src/kernel/Process.cpp \
- /home/victorkelly1/C++/practicas/RTOS/include/kernel/Process.hpp \
- /usr/lib/gcc/avr/7.3.0/include/stddef.h \
- /usr/lib/gcc/avr/7.3.0/include/stdint.h /usr/lib/avr/include/stdint.h
